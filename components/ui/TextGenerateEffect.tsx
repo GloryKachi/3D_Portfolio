db4748a0c174a7dc8358cebe5ef7f9207b46a089
GloryKachi <kachikachina@gmail.com>
2025-03-02 08:18:57 +0100

    Initial commit

---
+++ components/ui/TextGenerateEffect.tsx
@@ -0,0 +1,74 @@
+"use client";
+import { useEffect } from "react";
+import { motion, stagger, useAnimate } from "framer-motion";
+import { cn } from "@/lib/utils";
+
+export const TextGenerateEffect = ({
+  words,
+  className,
+  filter = true,
+  duration = 0.5,
+}: {
+  words: string;
+  className?: string;
+  filter?: boolean;
+  duration?: number;
+}) => {
+  const [scope, animate] = useAnimate();
+  let wordsArray = words.split(" ").reduce((acc: string[], word, idx, arr) => {
+    if (word === "User" && arr[idx + 1] === "Experiences") {
+      acc.push("User Experiences");
+      arr.splice(idx + 1, 1);
+    } else {
+      acc.push(word);
+    }
+    return acc;
+  }, []);
+  useEffect(() => {
+    animate(
+      "span",
+      {
+        opacity: 1,
+        filter: filter ? "blur(0px)" : "none",
+      },
+      {
+        duration: duration ? duration : 1,
+        delay: stagger(0.2),
+      }
+    );
+  }, [scope.current]);
+
+  const renderWords = () => {
+    return (
+      <motion.div ref={scope}>
+        {wordsArray.map((word, idx) => {
+          return (
+            <motion.span
+              key={word + idx}
+              className={`${
+                word === "User Experiences"
+                  ? "text-purple"
+                  : "dark:text-white text-black"
+              } opacity-0`}
+              style={{
+                filter: filter ? "blur(10px)" : "none",
+              }}
+            >
+              {word}{" "}
+            </motion.span>
+          );
+        })}
+      </motion.div>
+    );
+  };
+
+  return (
+    <div className={cn("font-bold", className)}>
+      <div className="my-4">
+        <div className=" dark:text-white text-black  leading-snug tracking-wide">
+          {renderWords()}
+        </div>
+      </div>
+    </div>
+  );
+};
